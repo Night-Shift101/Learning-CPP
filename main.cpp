9cd37ec259d50b11c097e346b688636743e3a38a
Night-Shift101 <24gavinfox@gmail.com>
2025-06-05 19:00:30 -0400

    Switched IDEs Testing

--- main.cpp
+++ main.cpp
@@ -1,36 +1,28 @@
-#include <iostream>
-#include <random>
-using namespace std;
+#include <wx/wx.h>
 
-
-
-int main()
+class MyApp : public wxApp
 {
-    random_device rd; 
-    mt19937 generator(rd());
-    uniform_int_distribution<int> distribution(1, 100);
-    int secret_number = distribution(generator);
-    int attempts = 0;
-    while (true) {
-        int guess;
-        attempts++;
-        cout << "Guess the secret number (between 1 and 100): ";
-        cin >> guess;
+public:
+    virtual bool OnInit();
+};
 
-        if (guess < 1 || guess > 100) {
-            cout << "Please enter a number between 1 and 100." << endl;
-            continue;
-        }
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame(const wxString& title);
+};
 
-        if (guess < secret_number) {
-            cout << "Too low! Try again." << endl;
-        } else if (guess > secret_number) {
-            cout << "Too high! Try again." << endl;
-        } else {
-            cout << "Congratulations! You've guessed the secret number: " << secret_number << endl;
-            cout << "It took you " << attempts << " attempts." << endl;
-            break;
-        }
-    }
-    return 0;
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    MyFrame* frame = new MyFrame("Blank UI");
+    frame->Show(true);
+    return true;
 }
+
+MyFrame::MyFrame(const wxString& title)
+    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 600))
+{
+}
+
